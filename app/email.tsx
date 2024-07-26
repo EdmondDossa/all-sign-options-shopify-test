360cf191cef7bba2cb769240622819cf53e9d5b9
Maxpro-dk <toyigbemaximed@gmail.com>
2024-07-26 17:38:32 +0100

    gestion des erreurs de build

--- app/email.tsx
+++ app/email.tsx
@@ -1,9 +1,9 @@
 import { render } from '@react-email/render';
-import { createTransport } from 'nodemailer';
 import { RecapMail } from './components/emails/RecapMail';
+import  * as nodemailer  from   'nodemailer'
 
 
-const transporter = createTransport({
+const transporter = nodemailer.createTransport({
   host: 'smtp-view.alwaysdata.net',
   port: 587,
   secure: false,
