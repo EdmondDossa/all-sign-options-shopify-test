8283165f3544c53146f65fe07e5ce7b794c874e5
Maxpro-dk <toyigbemaximed@gmail.com>
2024-08-14 15:09:08 +0100

    corriger le mail

--- app/email.tsx
+++ app/email.tsx
@@ -1,17 +1,28 @@
 import { render } from '@react-email/render';
 import { RecapMail } from './components/emails/RecapMail';
-import  * as nodemailer  from   'nodemailer'
+import * as nodemailer from 'nodemailer'
 
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp-view.alwaysdata.net',
-  port: 587,
-  secure: false,
+  host: (process.env.EMAIL_OUTGOING_SERVER || 'signsdesigner.us'),
+  port: (process.env.EMAIL_SMTP_PORT || 465),
+  secure:( process.env.EMAIL_AUTHENTICATION_REQUIRED ? process.env.EMAIL_AUTHENTICATION_REQUIRED: true),
   auth: {
-    user: 'view@alwaysdata.net',
-    pass: 'F3QG3x7@QuPXtZx',
+    user: (process.env.EMAIL_USERNAME || 'support@signsdesigner.us'),
+    pass:( process.env.EMAIL_PASSWORD || 'NmLHKU#yrU*i'),
   },
 });
+
+
+
+export async function testMail(){
+  transporter.sendMail({
+    from:  `"${ process.env.EMAIL_NAME || 'All  Signs options support'}" <${ process.env.EMAIL_USERNAME || 'support@signsdesigner.us'}>`,
+    to: "toyigbemaximed@gmail.com",
+    subject: 'test my email',
+    text: 'test',
+  });
+}
 
 
 
@@ -21,7 +32,7 @@ export async function sendRecapMail(data:any, email:string, subject:string,custo
     const emailHtml = render(<RecapMail data={data} customer={customer}/>);
 
     let options :any= {
-      from: 'view@alwaysdata.net',
+      from: `"${ process.env.EMAIL_NAME || 'All  Signs options support'}" <${ process.env.EMAIL_USERNAME || 'support@signsdesigner.us'}>`,
       to: email,
       subject: subject,
       html: emailHtml
