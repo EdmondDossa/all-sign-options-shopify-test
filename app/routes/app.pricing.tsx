ae350adb86e6d962aee74767369d0ebef5ae8df2
Maxpro-dk <toyigbemaximed@gmail.com>
2025-01-24 11:55:44 +0100

    centraliser  la donné  des  prix

--- app/routes/app.pricing.tsx
+++ app/routes/app.pricing.tsx
@@ -15,7 +15,7 @@ import {
   } from "@shopify/polaris";
   import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
   import { useActionData, useLoaderData, useSubmit } from "@remix-run/react";
-import { authenticate, MONTHLY_PRO_PLAN, MONTHLY_STARTER_PLAN, YEARLY_PRO_PLAN, YEARLY_STARTER_PLAN } from "../shopify.server";
+import { authenticate, MONTHLY_PRO_PLAN, MONTHLY_STARTER_PLAN, PLAN_PRICES, YEARLY_PRO_PLAN, YEARLY_STARTER_PLAN } from "../shopify.server";
 import {
     CheckIcon,XIcon
   } from '@shopify/polaris-icons';
@@ -68,8 +68,8 @@ import { useId } from "react";
     {
       title: "Basic",
       description: "Basic  with basic features",
-      price: "39",
-      year_price: "375",
+      price: PLAN_PRICES.MONTHLY_STARTER_PLAN,
+      year_price: PLAN_PRICES.YEARLY_STARTER_PLAN,
       action: " Subscribe to Monthly",
       year_action: "Subscribe to Yearly",
       name: "starter",
@@ -92,8 +92,8 @@ import { useId } from "react";
     {
       title: "Premium",
       description: "Premium  with advanced features",
-      price: "79",
-      year_price: "759",
+      price: PLAN_PRICES.MONTHLY_PRO_PLAN,
+      year_price: PLAN_PRICES.YEARLY_PRO_PLAN,
       name: "pro",
       action: " Subscribe to Monthly",
       year_action: "Subscribe to Yearly",
