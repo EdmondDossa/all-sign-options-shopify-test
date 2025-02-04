97e76553b51be64f58ebdc3dfcacd429cb4850a9
Maxpro-dk <toyigbemaximed@gmail.com>
2025-02-04 10:25:26 +0100

    mise  à jours 1.0.30

--- app/routes/api.configurations.$id.ts
+++ app/routes/api.configurations.$id.ts
@@ -49,18 +49,18 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
     let configs = await ConfigurationService.getConfigurations(sessionId, true);
 
     let config = null;
-    plan = "free";
 
-    if (plan =="free" || !configs) {
+    if (!configs) {
         return json(null);
     }
+
+    config = configs?.find((curr: any) => curr.id == params.id)
+
     
     if (plan == PRICING_PLANS.STARTER) {
         configs = configs?.slice(0, PRICING_PLANS.STARTER_RULES.configurations)
         config = configs?.find((curr: any) => curr.id == params.id)
-    } else {
-        config = configs?.find((curr: any) => curr.id == params.id)
-    }
+    } 
 
     
 
@@ -68,9 +68,6 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
         config = configFilter(config);
     }
 
-   
-
-    
     return json(config );
 };
   
