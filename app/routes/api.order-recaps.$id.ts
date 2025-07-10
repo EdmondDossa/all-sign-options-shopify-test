86a10933e7516a4c778feca8898b0902da569fd9
Maxpro-dk <toyigbemaximed@gmail.com>
2025-07-10 10:55:29 +0100

    fix:  correction de  l'ajout de  shape

--- app/routes/api.order-recaps.$id.ts
+++ app/routes/api.order-recaps.$id.ts
@@ -7,9 +7,15 @@ import { authenticate } from "~/shopify.server";
 
 export const loader = async ({ request, params }:LoaderFunctionArgs) => {
     const { admin, session, cors } = await authenticate.admin(request);
-   
   
-    const  order =  await ShopifyOrderService.getOrder(admin, parseInt( params.id||"0") );
+    if (!params.id) {
+      return cors(json({
+        message: "id  de  la  commandes  est invalide",
+        statut:"error"
+      }))
+    }
+  
+    const  order =  await ShopifyOrderService.getOrder(admin, parseInt( params.id) );
   
     const variantsData:any =  []
     if (order) {
--- app/routes/api.order-uploads.$id.ts
+++ app/routes/api.order-uploads.$id.ts
@@ -7,8 +7,14 @@ import { authenticate } from "~/shopify.server";
 export const loader = async ({ request, params }:LoaderFunctionArgs) => {
     const { admin, session, cors } = await authenticate.admin(request);
    
+    if (!params.id) {
+      return cors(json({
+        message: "id  de  la  commandes  est invalide",
+        statut:"error"
+      }))
+    }
   
-    const  order:any =  await ShopifyOrderService.getOrder(admin, parseInt( params.id||"0") );
+    const  order:any =  await ShopifyOrderService.getOrder(admin, parseInt(params.id) );
     const designs =  await DesignService.getDesignByOrderId(`${params.id}`, session.id)
   
     return  cors(json({
