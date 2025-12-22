a2a1c73bbd6894e8b8b4bfa900d040adfa64fde4
Cedrickogb <cedrickogoubiyi13@gmail.com>
2025-12-22 09:50:07 +0100

    Changement du nom "All signs options" en "All signs customizer"

--- extensions/aso-admin-order-detail/src/BlockExtension.tsx
+++ extensions/aso-admin-order-detail/src/BlockExtension.tsx
@@ -66,7 +66,8 @@ function App() {
 
   return (
     // The AdminBlock component provides an API for setting the title of the Block extension wrapper.
-    <AdminBlock title="ALL SIGNS OPTIONS 2">
+    // <AdminBlock title="ALL SIGNS OPTIONS 2">
+    <AdminBlock title="ALL SIGNS CUSTOMIZER">
       <BlockStack gap="small small">
         <InlineStack blockAlignment="end">
 
