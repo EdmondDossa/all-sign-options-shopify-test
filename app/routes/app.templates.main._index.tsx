f44a959f2583f057b57ff9bd62962a6ead2c1977
Bernadoadk <bernado.adikpeto@epitech.eu>
2026-02-09 15:51:17 +0100

    update

--- app/routes/app.templates.main._index.tsx
+++ app/routes/app.templates.main._index.tsx
@@ -281,12 +281,12 @@ export default function ConfigurationTemplates() {
                   </InlineStack>
                 </Box>
               </button>
-              {/*<ComingSoonButton />*/}
-              <button
+              <ComingSoonButton />
+              {/*<button
                 className="primary-btn"
                 type="button"
                 onClick={() => { onHandlePacks() }}
-              >
+              >*/}
                 <Box paddingInline="100">
                   <InlineStack gap="100">
                     <PlusIcon />
@@ -371,7 +371,7 @@ export default function ConfigurationTemplates() {
       </div>
 
 
-    </div>
+    </div >
 
   );
 };
